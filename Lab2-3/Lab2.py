e305249795f16f170f872276dcd79e4d58cba40b
Jamal Mahmoud <bluedocjamal@hotmail.co.uk>
2017-02-06 14:15:58 +0000

    Finished Categorical Features

--- Lab2-3/Lab2.py
+++ Lab2-3/Lab2.py
@@ -12,23 +12,25 @@ Desc:   This program collects a dataset
 
 """
 
+
 def main():
-    #File Locations
+    # File Locations
     dataFile = '.\data\DataSet.txt'
     headerFile = r'.\data\featureNames.txt'
 
     """gets header names"""
-    f =  open(headerFile, 'r')
+    f = open(headerFile, 'r')
     header = f.read().splitlines()
+    f.close()
 
     """gets file as a csv"""
     dataFrame = GetRawCSV(dataFile, header)
 
     """Split dataframe into continuous and categorical features"""
-    #hardcoded columns for continuous and categorical
-    splice = [[1,9,10,11,12,13,16,18,19,20,21,22,23,24,25],[0,2,3,4,5,6,7,8,14,15,17]]
+    # hardcoded columns for continuous and categorical
+    splice = [[1, 9, 10, 11, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 25], [0, 2, 3, 4, 5, 6, 7, 8, 14, 15, 17]]
 
-    #pass in indices and dataframe to be spliced
+    # pass in indices and dataframe to be spliced
     featureSplit = SpliceFeatures(dataFrame, splice)
 
     """Create DQR's for continuous and Categorical Features"""
@@ -36,13 +38,16 @@ def main():
     Cat_Report = CategoricalFeatures(featureSplit[1])
 
     """ Export file to reports directory as csv/txt"""
-    Cont_Report.to_csv('./reports/c13730921.txt')
+    Cont_Report.to_csv('./reports/c13730921Continuous.txt')
+    Cat_Report.to_csv('./reports/c13730921Categorical.txt')
 
 
 """---------------Continuous Features---------------------------"""
-#method to prepare continuous feature DQR
+
+
+# method to prepare continuous feature DQR
 def ContinuousFeatures(dataFrame):
-    #columns in DQR
+    # columns in DQR
     minimum = []
     card = []
     count = []
@@ -54,18 +59,17 @@ def ContinuousFeatures(dataFrame):
     maximum = []
     sd = []
 
-    #populate columns in DQR
+    # populate columns in DQR
     for i in dataFrame.columns:
         count.append(len(dataFrame[i]))
-
-        dataFrame[i] = dataFrame[i].replace("?", numpy.nan)
+        dataFrame[i] = CleanData(dataFrame[i], "?")
         dataFrame[i] = dataFrame[i].apply(float)
 
         missing = len(dataFrame[i]) - dataFrame[i].count()
         missPercent.append(percentage(missing, len(dataFrame[i])))
 
         minimum.append(dataFrame[i].min())
-        card.append(str(len(dataFrame[i].unique())))
+        card.append(Cardinality(dataFrame[i]))
         mean.append(dataFrame[i].mean())
         median.append(dataFrame[i].median())
         maximum.append(dataFrame[i].max())
@@ -73,50 +77,95 @@ def ContinuousFeatures(dataFrame):
         quart1.append(dataFrame[i].quantile(0.25))
         quart3.append(dataFrame[i].quantile(0.75))
 
-    #zip data together and create dataframe with column names
+    # zip data together and create dataframe with column names
     dataSet = list(zip(count, missPercent, card, minimum, quart1, mean, median, quart3, maximum, sd))
-    columnSet = ['count' ,'Missing Percentage', 'Cardinality', 'Minimum', '1st Quartile', 'Mean', 'Median', '3rd Quartile', 'Maximum', 'Standard Deviation']
+    columnSet = ['count', 'Missing Percentage', 'Cardinality', 'Minimum', '1st Quartile', 'Mean', 'Median',
+                 '3rd Quartile', 'Maximum', 'Standard Deviation']
 
-    df = pd.DataFrame(data=dataSet ,index=dataFrame.columns, columns=columnSet)
+    df = pd.DataFrame(data=dataSet, index=dataFrame.columns, columns=columnSet)
     return df
 
+
 """----------------- Categorical Features -------------------"""
+
+
 def CategoricalFeatures(dataFrame):
+    count = []
     mode = []
     modePercent = []
+    modeFreq = []
     mode2 = []
     mode2Percent = []
+    mode2Freq = []
     missPercent = []
     card = []
 
     for i in dataFrame.columns:
-        mode.append(dataFrame[i].mode())
-        mode2.append(SecondMode(dataFrame[i]))
-    #print mode
+        count.append(len(dataFrame[i]))
+        # replaces missing data with NaN values
+        dataFrame[i] = CleanData(dataFrame[i], "?")
+
+        missing = len(dataFrame[i]) - dataFrame[i].count()
+        print missing
+        missPercent.append(percentage(missing, len(dataFrame[i])))
+
+        mode.append(Mode(dataFrame[i], 0))
+        modeFreq.append(FrequencyMode(0, dataFrame[i]))
+        modePercent.append(percentage(FrequencyMode(0, dataFrame[i]), len(dataFrame[i])))
+
+        mode2.append(Mode(dataFrame[i], 1))
+        mode2Freq.append(FrequencyMode(1, dataFrame[i]))
+        mode2Percent.append(percentage(FrequencyMode(1, dataFrame[i]), len(dataFrame[i])))
+        card.append(Cardinality(dataFrame[i]))
+    # print count
+    # print missPercent
+    # print dataFrame['num-of-doors']
+
+    dataSet = list(zip(count, missPercent, card, mode, modeFreq, modePercent, mode2, mode2Freq, mode2Percent))
+    columnSet = ['Count', 'Missing Percentage', 'Cardinality', 'Mode', 'Mode Frequency', 'Mode Percentage', '2nd Mode',
+                 '2nd Mode Frequency', '2nd Mode Percentage']
+    df = pd.DataFrame(data=dataSet, index=dataFrame.columns, columns=columnSet)
+    # print df
+    return df
 
 
 """Some functions to keep methods clean"""
 
-#splits up continuous and categorical data
+
+# splits up continuous and categorical data
 def SpliceFeatures(dataFrame, splice):
-    #splice dataframe into 2 tables
-    continuous = dataFrame.iloc[:,splice[0]]
-    categorical = dataFrame.iloc[:,splice[1]]
+    # splice dataframe into 2 tables
+    continuous = dataFrame.iloc[:, splice[0]]
+    categorical = dataFrame.iloc[:, splice[1]]
     return [continuous, categorical]
 
-#percentage function to keep methods clean
+
+# percentage function to keep methods clean
 def percentage(part, whole):
     return 100 * float(part) / float(whole)
 
-def SecondMode(dataSet):
+
+def Cardinality(dataFrame):
+    return str(len(dataFrame.unique()))
+
+
+def CleanData(dataFrame, character):
+    return dataFrame.replace("?", numpy.nan)
+
+
+def FrequencyMode(mode, dataFrame):
+    commonValues = dataFrame.value_counts()
+
+    return commonValues.iloc[mode]
+
+def Mode(dataSet, mode):
     commonValue = dataSet.value_counts()
-    #for i in range(len(dataSet)):
+    return commonValue.index[mode]
 
-    return commonValue.index[1]
-
-#reads txt file from data folder adn returns it as a pandas.DataFrame
+# reads txt file from data folder adn returns it as a pandas.DataFrame
 def GetRawCSV(path, header):
     return pd.read_csv(path, names=header)
+
 
 if __name__ == '__main__':
     main()
